beb1186f258ee821599148133f54f8da2437d4c0
umdelii <sang9205@gmail.com>
2025-12-02 18:12:15 +0900

    crud마지막

--- student/src/main/java/com/example/student/entity/Student.java
+++ student/src/main/java/com/example/student/entity/Student.java
@@ -2,7 +2,6 @@ package com.example.student.entity;
 
 import java.time.LocalDateTime;
 
-import org.hibernate.annotations.CreationTimestamp;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
